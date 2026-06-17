2b96e23e5470d8491c9599e905ddef6fa0f69936
Omri Melcer <omelcer@gmail.com>
2026-06-17 19:18:22 +0300

    continue sovling this bs

--- src/VirtualMemory.cpp
+++ src/VirtualMemory.cpp
@@ -17,29 +17,57 @@ uint64_t extract_bits(uint64_t num, int start, int end)
   return (num & mask) >> start;
 }
 
+word_t page_fault_handler(uint64_t *virtualAddress, word_t prev_addr)
+{
+  // Handle page fault (e.g., allocate a new physical frame, update page tables,
+  // etc.) This is a placeholder implementation and should be replaced with
+  // actual logic. For example, you might want to allocate a new physical frame
+  // and update the page table entry.
+  return 0;
+}
+
 word_t down_the_rabit_hole(uint64_t virtualAdress)
 {
 
   int start_of_first_adress= TABLES_DEPTH * OFFSET_WIDTH;
   uint64_t first_page_index= extract_bits(virtualAdress, start_of_first_adress,
                                           VIRTUAL_ADDRESS_WIDTH - 1);
-  word_t next_addr;
-  PMread(first_page_index, &next_addr);
-  for (int i= 1; i < TABLES_DEPTH; i++)
+  word_t next_addr, tmp, prev_addr;
+  PMread(first_page_index, &tmp);
+  next_addr= tmp;
+  for (int i= 1; i < TABLES_DEPTH - 1; i++)
   {
+    if (next_addr == 0)
+    {
+      page_fault_handler(&virtualAdress, prev_addr);
+    }
     uint64_t page_index=
         extract_bits(virtualAdress, OFFSET_WIDTH + i * OFFSET_WIDTH,
                      OFFSET_WIDTH + (i + 1) * OFFSET_WIDTH - 1);
-    uint64_t physical_frame_index= VMgetMapping(page_index);
-    if (physical_frame_index == 0)
-    {
-      return 0;
-    }
-    virtualAdress= physical_frame_index * PAGE_SIZE;
+    PMread(next_addr * PAGE_SIZE + page_index, &tmp);
+    prev_addr= next_addr * PAGE_SIZE + page_index;
+    next_addr= tmp;
   }
+  // final stop is the actual phrame of the certain page(given that it exists)
+  return next_addr;
 }
 
-int VMread(uint64_t virtualAddress, word_t *value);
+int VMread(uint64_t virtualAddress, word_t *value)
+{
+  /**
+   * Read algorithm:
+   * 1. receive a page. in VM format. (that what was allocated).
+   * 2. if finish DFS and found the page, return the value.
+   * 3. if encountered a 0 on the way - page fault, assume all memories are
+   * readable from disk even untouched. fault handler.
+   *  */
+
+  if (virtualAddress < 0 || virtualAddress >= VIRTUAL_MEMORY_SIZE)
+  {
+    return 1;
+  }
+  return 0;
+}
 
 int VMwrite(uint64_t virtualAddress, word_t value);
 
